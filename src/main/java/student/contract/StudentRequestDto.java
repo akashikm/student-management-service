97a10fe494ecaaea910b5f194b3069be6f047b1a
Akash V U <akash@ikm.gov.in>
2026-01-09 17:18:35 +0530

    Update StudentRequestDto.java

--- src/main/java/student/contract/StudentRequestDto.java
+++ src/main/java/student/contract/StudentRequestDto.java
@@ -10,7 +10,11 @@ import lombok.*;
 @AllArgsConstructor
 public class StudentRequestDto {
     private String name;
+    @Min(value = 3, message = "Age must be at least 3")
+    @Max(value = 30, message = "Age must be at most 30")
     private Integer age;
     private String course;
+    @Email(message = "Email must be valid")
     private String email;
 }
+
